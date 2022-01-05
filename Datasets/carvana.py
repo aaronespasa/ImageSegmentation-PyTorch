24b59311153b030483f3e7713eec1304fb699ac6
aaronespasa <aaespasa@gmail.com>
2022-01-05 16:49:16 +0100

    Creating a folder to store the datasets

---
+++ Datasets/carvana.py
@@ -0,0 +1,10 @@
+"""
+This dataset can be found using the following URL:
+https://www.kaggle.com/c/carvana-image-masking-challenge
+
+Copyright (c), Aarón Espasandín - All Rights Reserved
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree:
+https://github.com/aaronespasa/ImageSegmentation-PyTorch/blob/main/LICENSE
+"""
